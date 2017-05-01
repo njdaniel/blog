4da33b1b04239249142e7198ecf99ea48244199b
njdaniel <nicholasjdaniel@gmail.com>
2017-05-01 15:57:21 -0400

    setup default redux

--- src/index.js
+++ src/index.js
@@ -2,8 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+
+import reducers from './reducers';
+
+const createStoreWithMiddleware = applyMiddleware()(createStore);
 
 ReactDOM.render(
-  <App />,
+    <Provider store={createStoreWithMiddleware(reducers)}>
+        <App />
+    </Provider>
+  ,
   document.getElementById('root')
 );
