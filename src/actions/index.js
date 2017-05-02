f58cd3b858970f32ea75a6e212f03b0ca2c57575
njdaniel <nicholasjdaniel@gmail.com>
2017-05-02 10:51:05 -0400

    add get action

---
+++ src/actions/index.js
@@ -0,0 +1,18 @@
+/**
+ * Created by nicholas on 5/2/17.
+ */
+import axios from 'axios';
+
+
+export const FETCH_POSTS = 'fetch_posts';
+
+const ROOT_URL = 'http://reduxblog.herokuapp.com/api';
+const API_KEY = '?key=1337sheep';
+
+export function fetchPosts() {
+    const request = axios.get(`${ROOT_URL}/posts${API_KEY}`);
+    return {
+      type: FETCH_POSTS,
+        payload: request
+    };
+}
