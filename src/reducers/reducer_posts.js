7f0890198fe4eca58ff247b64200372295d7395d
njdaniel <nicholasjdaniel@gmail.com>
2017-05-02 11:24:32 -0400

    convert array to object for posts

--- src/reducers/reducer_posts.js
+++ src/reducers/reducer_posts.js
@@ -1,13 +1,13 @@
 /**
  * Created by nicholas on 5/2/17.
  */
+import _ from 'lodash';
 import {FETCH_POSTS} from '../actions';
 
 export default function (state = {}, action) {
     switch (action.type) {
         case FETCH_POSTS:
-            console.log(action.payload.data); // [post1, post2]
-            // {4: post }
+            return _.mapKeys(action.payload.data, 'id');
 
         default:
             return state;
