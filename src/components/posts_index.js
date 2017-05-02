053deafbfca875a1250df2618dcbd03ca47ea527
njdaniel <nicholasjdaniel@gmail.com>
2017-05-02 11:33:00 -0400

    ajax call

--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -2,8 +2,14 @@
  * Created by nicholas on 5/2/17.
  */
 import React, {Component} from 'react';
+import {connect} from 'react-redux';
+import {fetchPosts} from '../actions';
 
 class PostsIndex extends Component {
+    componentDidMount() {
+        this.props.fetchPosts();
+    }
+
     render() {
         return (
             <div>
@@ -13,4 +19,4 @@ class PostsIndex extends Component {
     }
 }
 
-export default PostsIndex;
+export default connect(null, {fetchPosts})(PostsIndex);
