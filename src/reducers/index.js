298a4ed765c00bab9fe629c9c5bf9ef195da465f
njdaniel <nicholasjdaniel@gmail.com>
2017-05-02 11:12:55 -0400

    add reducer post

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,10 +2,11 @@
  * Created by nicholas on 5/1/17.
  */
 import {combineReducers} from 'redux';
+import PostsReducer from './reducer_posts';
 
 
 const rootReducer = combineReducers({
-
+    posts: PostsReducer
 });
 
 export default rootReducer;
