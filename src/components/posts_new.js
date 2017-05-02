72bacfbf05da1f6bd7a9370f870ba966f99cc449
njdaniel <nicholasjdaniel@gmail.com>
2017-05-02 12:49:42 -0400

    add new post form

---
+++ src/components/posts_new.js
@@ -0,0 +1,16 @@
+/**
+ * Created by nicholas on 5/2/17.
+ */
+import React, {Component} from 'react';
+
+class PostsNew extends Component {
+    render() {
+        return (
+          <div>
+              PostsNew!
+          </div>
+        );
+    }
+}
+
+export default PostsNew;
