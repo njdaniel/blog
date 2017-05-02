d86ed60fc24575e43bf88fc0e86cedd76e72e42f
njdaniel <nicholasjdaniel@gmail.com>
2017-05-02 14:50:21 -0400

    add redux form

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,11 +2,13 @@
  * Created by nicholas on 5/1/17.
  */
 import {combineReducers} from 'redux';
+import {reducer as formReducer} from 'redux-form';
 import PostsReducer from './reducer_posts';
 
 
 const rootReducer = combineReducers({
-    posts: PostsReducer
+    posts: PostsReducer,
+    form: formReducer
 });
 
 export default rootReducer;
