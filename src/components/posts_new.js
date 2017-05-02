bb8b6932dd9245af997d3327e7aaff8734d41a39
njdaniel <nicholasjdaniel@gmail.com>
2017-05-02 15:02:57 -0400

    add redux form helper

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -2,6 +2,7 @@
  * Created by nicholas on 5/2/17.
  */
 import React, {Component} from 'react';
+import {Field, reduxForm} from 'redux-form';
 
 class PostsNew extends Component {
     render() {
@@ -13,4 +14,6 @@ class PostsNew extends Component {
     }
 }
 
-export default PostsNew;
+export default reduxForm({
+    form: 'PostsNewForm'
+})(PostsNew);
