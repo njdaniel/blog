e379d1fc0fd76111859de2673867a0acd215200a
njdaniel <nicholasjdaniel@gmail.com>
2017-05-03 10:55:01 -0400

    add Post show

---
+++ src/components/posts_show.js
@@ -0,0 +1,16 @@
+/**
+ * Created by nicholas on 5/3/17.
+ */
+import React, { Component } from 'react';
+
+class PostsShow extends Component {
+    render() {
+        return (
+            <div>
+                Posts Show!
+            </div>
+        );
+    };
+}
+
+export default PostsShow;
