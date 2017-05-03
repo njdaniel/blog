152faae668dee9167adb26dbcb8c760672a57004
njdaniel <nicholasjdaniel@gmail.com>
2017-05-03 11:50:44 -0400

    get one post

--- src/reducers/reducer_posts.js
+++ src/reducers/reducer_posts.js
@@ -2,10 +2,15 @@
  * Created by nicholas on 5/2/17.
  */
 import _ from 'lodash';
-import {FETCH_POSTS} from '../actions';
+import {FETCH_POSTS, FETCH_POST} from '../actions';
 
 export default function (state = {}, action) {
     switch (action.type) {
+        case FETCH_POST:
+            const post = action.payload.data;
+            const newState = { ...state };
+            newState[post.id] = post;
+            return newState;
         case FETCH_POSTS:
             return _.mapKeys(action.payload.data, 'id');
 
