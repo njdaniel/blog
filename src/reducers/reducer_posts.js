037b5f48f18f2c4bf0979effecbb38aeeda91836
njdaniel <nicholasjdaniel@gmail.com>
2017-05-03 11:52:54 -0400

    refactor

--- src/reducers/reducer_posts.js
+++ src/reducers/reducer_posts.js
@@ -7,10 +7,12 @@ import {FETCH_POSTS, FETCH_POST} from '../actions';
 export default function (state = {}, action) {
     switch (action.type) {
         case FETCH_POST:
-            const post = action.payload.data;
-            const newState = { ...state };
-            newState[post.id] = post;
-            return newState;
+            // const post = action.payload.data;
+            // const newState = { ...state };
+            // newState[post.id] = post;
+            // return newState;
+            // es6 refactor
+            return { ...state, [action.payload.data.id]: action.payload.data };
         case FETCH_POSTS:
             return _.mapKeys(action.payload.data, 'id');
 
